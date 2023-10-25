a262823ee4cdeeee14df83c080526695089e8052
gonzalo00913 <gonzalo-masa@outlook.com>
2023-10-25 19:16:55 -0300

    🕒En curso

---
+++ Ejercicios_flujos_de_repeticion/ejercicio037.py
@@ -0,0 +1,7 @@
+for numero in range(1, 6):
+    print(numero)
+
+for numero in range(5, 0, -1):
+    print(numero)
+
+
---
+++ Ejercicios_flujos_de_repeticion/ejercicio038.py
@@ -0,0 +1,6 @@
+int(input("ingresa un numero : "))
+
+while n < 11:
+ n = n * 2
+
+ print(n)
